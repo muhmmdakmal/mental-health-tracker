75d6b16a3cf51181dcb0e13e57c74e82f8adbc72
muhmmdakmal <muhmmdakmal.69@gmail.com>
2024-08-28 16:23:53 +0700

    Mengupdate settings

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$3pmcn9l4wk@-mgu$p$2dhsfq+1q151$$h(7%fn$$ie^t33fhj
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://pbp.cs.ui.ac.id/muhammad-akmal36/mentalhealthtracker"]
 
 
 # Application definition
